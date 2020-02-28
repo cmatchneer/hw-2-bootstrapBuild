c8946164f67d96c61a87dfc6dd69e969d8865a4a
cmatchneer <cmatchneer@yahoo.com>
2020-02-28 17:05:27 -0500

    server set up issues

---
+++ public/assets/javascript/javascript.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(".navbar-toggler").click(function() {
+        $(".collapse").slideToggle(1000);
+
+    })
+    $("#submit").on("click", function() {
+
+        var userInput = {
+            fullName: $("#fullName").val().trim(),
+            companyName: $("#companyName").val().trim(),
+            email: $("#email").val().trim(),
+            questions: $("#questions").val().trim()
+        };
+
+    })
+})
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 3000;
+var path = require("path");
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+// var routes = require("./controllers/burger_controller");
+
+app.use(express.static(__dirname + "/public/assets/css"));
+app.use(express.static(__dirname + "/public/assets/javascript"));
+app.use(express.static(__dirname + "/public/assets/images"));
+
+console.log(__dirname + "/assets/css");
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require("./routes/htmlRoutes.js")(app);
+// require("./routes/author-api-routes.js")(app);
+// require("./routes/apiRoutes.js")(app);
+
+// app.use(routes);
+app.listen(PORT, function() {
+
+    console.log("Server listening on: http://localhost:" + PORT);
+});
