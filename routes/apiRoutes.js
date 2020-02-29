14d7313c65edfe42290e8ae78ce220a52c3fee0e
cmatchneer <cmatchneer@yahoo.com>
2020-02-28 19:04:36 -0500

    email issues are still happening

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,42 +3,93 @@ const nodemailer = require("nodemailer");
 module.exports = function(app) {
 
     app.post("/api/email", function(req, res) {
-        // async..await is not allowed in global scope, must use a wrapper
-        async function main() {
-            // Generate test SMTP service account from ethereal.email
-            // Only needed if you don't have a real mail account for testing
-            let testAccount = await nodemailer.createTestAccount();
+        /* eslint no-console: 0 */
 
-            // create reusable transporter object using the default SMTP transport
+        'use strict';
+
+
+
+        // Generate SMTP service account from ethereal.email
+        nodemailer.createTestAccount((err, account) => {
+            if (err) {
+                console.error('Failed to create a testing account');
+                console.error(err);
+                return process.exit(1);
+            }
+
+            console.log('Credentials obtained, sending message...');
+
+            // NB! Store the account object values somewhere if you want
+            // to re-use the same account for future mail deliveries
+
+            // Create a SMTP transporter object
             let transporter = nodemailer.createTransport({
-                host: "smtp.ethereal.email",
-                port: 587,
-                secure: false, // true for 465, false for other ports
+                host: account.smtp.host,
+                port: account.smtp.port,
+                secure: account.smtp.secure,
                 auth: {
-                    user: testAccount.user, // generated ethereal user
-                    pass: testAccount.pass // generated ethereal password
+                    user: account.user,
+                    pass: account.pass
+                },
+                logger: true,
+                debug: false // include SMTP traffic in the logs
+            }, {
+                // default message fields
+
+                // sender info
+                from: 'Nodemailer <example@nodemailer.com>',
+                headers: {
+                    'X-Laziness-level': 1000 // just an example header, no need to use this
                 }
             });
 
-            // send mail with defined transport object
-            let info = await transporter.sendMail({
-                from: req.body.email, // sender address
-                to: "cmatchneer@yahoo.com", // list of receivers
-                subject: req.body.companyName, // Subject line
-                text: req.body.fullName + " " + req.body.questions, // plain text body
-                html: "<b>Hello world?</b>" // html body
+            // Message object
+            let message = {
+                // Comma separated list of recipients
+                to: 'Nodemailer <cmatchneer@yahoo.com>',
+
+                // Subject of the message
+                subject: 'Nodemailer is unicode friendly ✔' + Date.now(),
+
+                // plaintext body
+                text: 'Hello to myself!',
+
+                // HTML body
+                html: `<p><b>Hello</b> to myself <img src="cid:note@example.com"/></p>
+        <p>Here's a nyan cat for you as an embedded attachment:<br/><img src="cid:nyan@example.com"/></p>`,
+
+                // AMP4EMAIL
+                amp: `<!doctype html>
+        <html ⚡4email>
+          <head>
+            <meta charset="utf-8">
+            <style amp4email-boilerplate>body{visibility:hidden}</style>
+            <script async src="https://cdn.ampproject.org/v0.js"></script>
+            <script async custom-element="amp-anim" src="https://cdn.ampproject.org/v0/amp-anim-0.1.js"></script>
+          </head>
+          <body>
+            <p><b>Hello</b> to myself <amp-img src="https://cldup.com/P0b1bUmEet.png" width="16" height="16"/></p>
+            <p>No embedded image attachments in AMP, so here's a linked nyan cat instead:<br/>
+              <amp-anim src="https://cldup.com/D72zpdwI-i.gif" width="500" height="350"/></p>
+          </body>
+        </html>`,
+
+            };
+
+            transporter.sendMail(message, (error, info) => {
+                if (error) {
+                    console.log('Error occurred');
+                    console.log(error.message);
+                    return process.exit(1);
+                }
+
+                console.log('Message sent successfully!');
+                console.log(nodemailer.getTestMessageUrl(info));
+
+                // only needed when using pooled connections
+                transporter.close();
             });
-
-            console.log("Message sent: %s", info.messageId);
-            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-            // Preview only available when sending through an Ethereal account
-            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-        }
-
-        main().catch(console.error);
-
+        });
         res.json("done");
     });
 }
