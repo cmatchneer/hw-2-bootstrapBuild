ace57a6a4570cb0dec983fef7c4df87abf93a33c
cmatchneer <cmatchneer@yahoo.com>
2020-05-13 19:25:28 -0400

    dropdown button installed and functional

--- public/assets/javascript/javascript.js
+++ public/assets/javascript/javascript.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
-    $(".navbar-toggler").click(function() {
-        $(".collapse").slideToggle(1000);
-
-    })
+    $(".dropdown-toggle").dropdown();
     $("#submit").on("click", function() {
 
         var userInput = {
