8397b512e65ee9c4541b54536aee21a287ebfa8e
cmatchneer <cmatchneer@yahoo.com>
2020-10-28 19:08:36 -0400

    added hangman to this removed contact for now

--- public/assets/javascript/javascript.js
+++ public/assets/javascript/javascript.js
@@ -5,21 +5,21 @@ $(document).ready(function() {
     });
     $("#submit").on("click", function() {
 
-        var userInput = {
-            fullName: $("#fullName").val().trim(),
-            companyName: $("#companyName").val().trim(),
-            email: $("#email").val().trim(),
-            questions: $("#questions").val().trim()
-        };
-        console.log(userInput.fullName.length)
-        if (userInput.fullName.length > 0 && userInput.companyName.length > 0 && userInput.email.length > 0 && userInput.questions.length > 0) {
-            console.log("test");
-            $.post("/api/email", userInput).then(function(response) {
-                console.log(response);
-            })
-        } else {
-            alert("please fill out whole form")
-        }
+        // var userInput = {
+        //     fullName: $("#fullName").val().trim(),
+        //     companyName: $("#companyName").val().trim(),
+        //     email: $("#email").val().trim(),
+        //     questions: $("#questions").val().trim()
+        // };
+        // console.log(userInput.fullName.length)
+        // if (userInput.fullName.length > 0 && userInput.companyName.length > 0 && userInput.email.length > 0 && userInput.questions.length > 0) {
+        //     console.log("test");
+        //     $.post("/api/email", userInput).then(function(response) {
+        //         console.log(response);
+        //     })
+        // } else {
+        //     alert("please fill out whole form")
+        // }
 
 
     })
